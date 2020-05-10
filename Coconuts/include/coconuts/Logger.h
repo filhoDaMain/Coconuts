b5171e6470d2ad37391738573c06f1daf826884a
filho_da_main <filho_da_main>
2020-05-10 14:18:12 +0100

    Making Logger::GetCoreLogger() and Logger::GetClientLogger inline functions

--- Coconuts/include/coconuts/Logger.h
+++ Coconuts/include/coconuts/Logger.h
@@ -27,8 +27,8 @@ namespace Coconuts
     public:
         static void Init();
         
-        static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-        static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+        inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+        inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
         
     private:
         static std::shared_ptr<spdlog::logger> s_CoreLogger;

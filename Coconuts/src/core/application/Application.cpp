0d03c16d6505c0044c4d1daf8e2fdbd4842cbb99
filho_da_main <filho_da_main>
2020-05-31 00:14:42 +0100

    Example of passing a vertex position from a Vertex Shader to a Fragment Shader

--- Coconuts/src/core/application/Application.cpp
+++ Coconuts/src/core/application/Application.cpp
@@ -119,11 +119,14 @@ namespace Coconuts
             
                 #version 330 core
                 
-                layout(location = 0) in vec3 a_Postion;
+                layout(location = 0) in vec3 a_Position;
+                
+                out vec3 v_Position;
                 
                 void main()
                 {
-                    gl_Position = vec4(a_Postion, 1.0);
+                    v_Position = a_Position;
+                    gl_Position = vec4(a_Position, 1.0);
                 }
                 
             )";
@@ -134,9 +137,11 @@ namespace Coconuts
                 
                 layout(location = 0) out vec4 color;
                 
+                in vec3 v_Position;
+                
                 void main()
                 {
-                    color = vec4(0.8, 0.7, 0.1, 1.0);
+                    color = vec4(v_Position, 1.0);
                 }
                 
             )";

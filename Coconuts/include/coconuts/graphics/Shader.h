8bca926ebf99cbdab1380d8a9a90242171675789
filho_da_main <filho_da_main>
2020-06-11 19:01:49 +0100

    Shader - Uniform for vec 4

--- Coconuts/include/coconuts/graphics/Shader.h
+++ Coconuts/include/coconuts/graphics/Shader.h
@@ -36,6 +36,7 @@ namespace Coconuts
         void Unbind();
         
         void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+        void UploadUniformFloat4(const std::string& name, const glm::vec4& values);
         
     private:
         uint32_t m_RendererID;
--- Coconuts/src/core/graphics/Shader.cpp
+++ Coconuts/src/core/graphics/Shader.cpp
@@ -178,4 +178,11 @@ namespace Coconuts
         glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
     }
     
+    void Shader::UploadUniformFloat4(const std::string& name, const glm::vec4& values)
+    {
+        GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+        
+        glUniform4f(location, values.x, values.y, values.z, values.w);
+    }
+    
 }

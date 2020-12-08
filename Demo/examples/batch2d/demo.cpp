59632ad2165d90b91f6bf394c3c67887c3dc31fa
filho_da_main <filho_da_main>
2020-12-08 17:06:36 +0000

    Oraganizing Demo project to contain multiple examples

--- Demo/examples/batch2d/demo.cpp
+++ Demo/examples/batch2d/demo.cpp
@@ -25,7 +25,6 @@
 
 #include <memory>
 #include <string>
-#include "demo.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
--- Demo/include/demo.h
+++
@@ -1,6 +0,0 @@
-#ifndef DEMO_H
-#define DEMO_H
-
-#define DEMO_VERSION 0.1F
-
-#endif

65899f31b29c1e1271e0b5291d79f70f2f834776
filhoDaMain <temprilho93@gmail.com>
2021-01-17 22:30:25 +0000

    New Panel: Asset Inspector (prototype)

---
+++ Coconuts/src/editor/standalone/panels/AssetInspector.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 Andre Temprilho
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "AssetInspector.h"
+#include <coconuts/editor.h>
+
+namespace Coconuts {
+namespace Panels
+{
+ 
+    bool AssetInspector::Init()
+    {
+        DrawContextFunc = [&](void) { this->DrawEmpty(); };
+        return true;
+    }
+    
+    void AssetInspector::Draw()
+    {
+        ImGui::Begin("Asset Inspector");
+        
+        /* Draw current context */
+        DrawContextFunc();
+        
+        ImGui::End();
+    }
+    
+    void AssetInspector::DrawEmpty()
+    {
+        //such empty :)
+    }
+    
+    void AssetInspector::DrawTexture2DAsset()
+    {
+        //TODO
+    }
+    
+    void AssetInspector::DrawSpriteAsset()
+    {
+        //TODO
+    }
+    
+    
+    /* Change Context - Texture2D */
+    void AssetInspector::ChangeContext2Texture2D(std::string& name)
+    {
+        DrawContextFunc = [&](void) { this->DrawTexture2DAsset(); };
+        m_LogicalNameTexture2D = name;
+    }
+    
+    /* Change Context - Sprite */
+    void AssetInspector::ChangeContext2Sprite(std::string& name)
+    {
+        DrawContextFunc = [&](void) { this->DrawSpriteAsset(); };
+        m_LogicalNameSprite = name;
+    }
+    
+}
+}
---
+++ Coconuts/src/editor/standalone/panels/AssetInspector.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 Andre Temprilho
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ASSETINSPECTOR_H
+#define ASSETINSPECTOR_H
+
+#include <string>
+#include <functional>
+
+namespace Coconuts {
+namespace Panels
+{
+ 
+    class AssetInspector
+    {
+    public:
+        AssetInspector() = default;
+        bool Init();
+        
+        void Draw();
+        
+        void ChangeContext2Texture2D(std::string& name);
+        void ChangeContext2Sprite(std::string& name);
+        
+    private:
+        std::function<void(void)> DrawContextFunc;
+        
+        void DrawTexture2DAsset();
+        void DrawSpriteAsset();
+        void DrawEmpty();
+        
+        /* Logical Names */
+        std::string m_LogicalNameTexture2D;
+        std::string m_LogicalNameSprite;
+        
+    };
+    
+}
+}
+
+#endif /* ASSETINSPECTOR_H */
+

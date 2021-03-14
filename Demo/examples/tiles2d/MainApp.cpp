d097074992089bb35f9c75dd03431fda3012691c
filhoDaMain <temprilho93@gmail.com>
2021-03-14 19:07:24 +0000

    Clean Demo folder - Remove old examples. Keep textures to be used in examples

--- Demo/examples/tiles2d/MainApp.cpp
+++
@@ -1,9 +0,0 @@
-#include "MainApp.h"
-#include "GameLayer.h"
-#include "EditorLayer.h"
-
-MainApp::MainApp()
-{
-    PushLayer( new GameLayer() );       // In-Game Layer
-    PushOverlay( new EditorLayer() );   // Editor UI Layer
-}

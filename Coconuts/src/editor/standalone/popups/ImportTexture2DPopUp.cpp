ea14439a901527095094fccdd7aa71a59c1d7478
filhoDaMain <temprilho93@gmail.com>
2021-03-24 23:12:40 +0000

    Import image as a Texture2D via Editor
    - Menu Bar - Assets: opens file dialog to select image file from filesystem
    - User confirms image import and AssetManager is called to create a Texture2D from selected file

--- Coconuts/src/editor/standalone/popups/ImportTexture2DPopUp.cpp
+++ Coconuts/src/editor/standalone/popups/ImportTexture2DPopUp.cpp
@@ -18,7 +18,9 @@
 #include <coconuts/editor.h>
 #include <coconuts/AssetManager.h>
 #include <coconuts/Logger.h>
+#include <nfd/nfd.h>
 #include <string>
+#include <cstring>
 
 namespace Coconuts {
 namespace PopUps
@@ -35,14 +37,40 @@ namespace PopUps
         if (ImGui::BeginPopupModal("AssetManager:  Import Texture 2D", NULL, ImGuiWindowFlags_AlwaysAutoResize))
         {            
             static char pathBuffer[200] = "/absolute/path/to/image.png";
-            static char logicalNameBuffer[32];
+            static char logicalNameBuffer[32] = "SingleWordName";
             
             /* Image Path */
             ImGui::Text("Select an image to import from filesystem");
             ImGui::Spacing();ImGui::Spacing();ImGui::Spacing();
-            if (ImGui::InputText("ABS PATH", pathBuffer, sizeof(pathBuffer)))
+            if (ImGui::InputText("", pathBuffer, sizeof(pathBuffer)))
             {
                 //
+            }
+            
+            ImGui::SameLine();
+            if (ImGui::Button("Open        "))
+            {
+                /* Open File Dialog */
+                nfdchar_t *outPath = NULL;
+                
+                /* WARNING -> outPath is dynamically allocated and needs to be freed after */
+                nfdresult_t result = NFD_OpenDialog( "png,jpg", NULL, &outPath );
+    
+                if ( result == NFD_OKAY )
+                {
+                    /* Copy to static buffer and free dynamically allocated outPath */
+                    strcpy(pathBuffer, outPath);
+                    free(outPath);  // NEEDED!
+                }
+                
+                else if ( result == NFD_CANCEL )
+                {
+                    //do nothing
+                }
+                else 
+                {
+                    LOG_WARN("Failed to load file from File Dialog");
+                }
             }
             
             ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
@@ -51,28 +79,45 @@ namespace PopUps
             /* Image Path */
             ImGui::Text("Give it a Logical Name to reference from code");
             ImGui::Spacing(); ImGui::Spacing(); ImGui::Spacing();
-            if (ImGui::InputText("LOGICAL NAME", logicalNameBuffer, sizeof(logicalNameBuffer)))
+            if (ImGui::InputText("Logical Name", logicalNameBuffer, sizeof(logicalNameBuffer)))
             {
                 //
             }
             
             ImGui::Separator();
 
-
+            
+            /* Import into AssetManager */
             if (ImGui::Button("Import", ImVec2(120, 0)))
             {
-                std::string path = std::string(pathBuffer);
-                std::string name = std::string(logicalNameBuffer);
-                AssetManager::ImportTexture2D(name, path);
+                if ( (strcmp(pathBuffer, "/absolute/path/to/image.png") != 0) && 
+                     (strcmp(logicalNameBuffer, "SingleWordName") != 0) )
+                {
+                    std::string path = std::string(pathBuffer);
+                    std::string name = std::string(logicalNameBuffer);
+                    AssetManager::ImportTexture2D(name, path);
+                    
+                    LOG_TRACE("Imported file {} into AssetManager as '{}'", path, name);
+                    
+                    /* Clear buffers for next usage */
+                    strcpy(pathBuffer, "/absolute/path/to/image.png");
+                    strcpy(logicalNameBuffer, "SingleWordName");
+                    
+                    ImGui::CloseCurrentPopup();
+                    *show = false;
+                }
                 
-                ImGui::CloseCurrentPopup();
-                *show = false;
+                LOG_WARN("Path to file or Logical Name not defined yet!");
             }
             ImGui::SetItemDefaultFocus();
             
             ImGui::SameLine();
             if (ImGui::Button("Cancel", ImVec2(120, 0)))
             {
+                /* Clear buffers for next usage */
+                strcpy(pathBuffer, "/absolute/path/to/image.png");
+                strcpy(logicalNameBuffer, "SingleWordName");
+                    
                 ImGui::CloseCurrentPopup();
                 *show = false;
             }

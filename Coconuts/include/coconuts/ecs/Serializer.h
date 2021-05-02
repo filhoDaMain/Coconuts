4dea9630961d27650e2db93ca0bddcc0f8f766df
filhoDaMain <temprilho93@gmail.com>
2021-05-02 23:34:34 +0100

    Add Serializer files to build tree
    - Constructor only

---
+++ Coconuts/include/coconuts/ecs/Serializer.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2021 Andre Temprilho
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef SERIALIZER_H
+#define SERIALIZER_H
+
+#include <memory>
+#include <coconuts/ecs/Scene.h>
+#include <coconuts/ecs/Entity.h>
+
+namespace Coconuts
+{
+    
+    class Serializer
+    {
+    public:
+        Serializer(const std::shared_ptr<Scene> scene);
+        ~Serializer() = default;
+        
+    private:
+        std::shared_ptr<Scene> m_Scene;
+    };
+    
+}
+
+#endif /* SERIALIZER_H */
+

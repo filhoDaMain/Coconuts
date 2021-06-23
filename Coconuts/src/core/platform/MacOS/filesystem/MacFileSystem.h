d4a6e64464a9f0f2d7e5438e1e10c9930cc3e277
André Temprilho <44137701+filhoDaMain@users.noreply.github.com>
2021-06-23 23:38:01 +0100

    Feature/filesystem tools (#93)
    * Add FileSystem interface class
    
    * Hotfix: Make FileSytem a singleton
    
    * Add FileSystem class interface
    
    - Add implementation for macOS platform;
    - Add implementation for GNU platform.

---
+++ Coconuts/src/core/platform/MacOS/filesystem/MacFileSystem.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 Andre Temprilho
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MACFILESYSTEM_H
+#define MACFILESYSTEM_H
+
+#include <coconuts/FileSystem.h>
+
+namespace Coconuts
+{
+    
+    namespace {
+    namespace Parser
+    {
+        namespace PATHS
+        {
+            constexpr auto RUNTIME_CONFDIR_REL_PATH = "";   // empty == same as binary's current location
+        }
+    }
+    } //namespace
+    
+    
+    class MacFileSystem : public FileSystem
+    {
+    public:
+        MacFileSystem() = default;
+        virtual ~MacFileSystem() = default;
+        
+    protected:
+        virtual std::string GetCurrDirPathImpl() override;
+        virtual std::string GetRuntimeConfDirPathImpl() override;
+    };
+    
+}
+
+#endif /* MACFILESYSTEM_H */
+
